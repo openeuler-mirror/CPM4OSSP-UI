7cf3386b22c52a5424928281d579dc9d95012d88
Dong Xia <dev12301@linx-info.com>
2023-02-02 02:26:30 +0000

    定义常量池
    Signed-off-by: Dong Xia <dev12301@linx-info.com>

---
+++ src/utils/const.js
@@ -0,0 +1,28 @@
+// const pool
+export const USER_NAME_KEY = 'Mpms-UserName'
+
+export const TOKEN_KEY = 'Mpms-Token'
+
+export const LONG_TERM_TOKEN = 'Mpms-Long-Term-Token'
+
+export const USER_INFO_KEY = 'Mpms-User'
+
+export const MENU_KEY = 'Mpms-Menus'
+
+export const TOKEN_HEADER_KEY = 'Authorization'
+
+export const ACTIVE_TAB_KEY = 'Mpms-ActiveTab'
+
+export const TAB_LIST_KEY = 'Mpms-TabList'
+
+export const ACTIVE_MENU_KEY = 'Mpms-ActiveMenu'
+
+export const GUIDE_FLAG_KEY = 'Mpms-GuideFlag'
+
+export const GUIDE_HOME_USED_KEY = 'Mpms-Home-Guide-Used'
+
+export const GUIDE_NODE_USED_KEY = 'Mpms-Node-Guide-Used'
+
+export const NO_NOTIFY_KEY = 'tip'
+
+export const NO_LOADING_KEY = 'loading'
