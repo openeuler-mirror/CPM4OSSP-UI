4fa7714cd0753d75926437ebb59fb346e9108087
Dong Xia <dev12301@linx-info.com>
2023-02-03 02:08:04 +0000

    完成webpack相关配置
    Signed-off-by: Dong Xia <dev12301@linx-info.com>

---
+++ vue.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  // 输出目录
+  outputDir: 'mpms',
+  // 控制静态资源使用相对路径
+  publicPath: './',
+  assetsDir: 'static',
+  // 本地服务器
+  devServer: {
+    port: 8080,
+    open: true
+  },
+  configureWebpack: {
+    name: '系统软件安装管理平台',
+    devtool: '#eval-source-map'
+  },
+  pluginOptions: {
+    // electron构建时的插件
+    electronBuilder: {
+      // nodeIntegration: true,
+      builderOptions: {
+        productName: 'mini-system'
+      },
+      icon: 'build/icons/32x32.png',
+      desktop: {
+        Icon: 'build/icons/32x32.png'
+      }
+    }
+  },
+  // 系统打开时的标题
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = '系统软件安装管理平台'
+      args[0].build = new Date().getTime()
+      return args
+    })
+  }
+}
