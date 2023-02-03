20ae0a30e0663f29150a8cf29ba02809a55e97d0
Dong Xia <dev12301@linx-info.com>
2023-02-03 05:12:05 +0000

    完成路由配置
    Signed-off-by: Dong Xia <dev12301@linx-info.com>

---
+++ src/router/route-menu.js
@@ -0,0 +1,14 @@
+/**
+ * 路由菜单
+ * key 对应后台接口返回的菜单中的 id
+ * value 表示该路由的 path
+ */
+const routeMenuMap = {
+  'nodeList': '/node/list',
+  'audit': '/audit/list',
+  'user': '/user/list',
+  'repository_source': '/repository/source',
+  'repository_package': '/repository/package'
+}
+
+export default routeMenuMap
