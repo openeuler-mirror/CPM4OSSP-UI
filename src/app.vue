d200569882c9fee1e13baedad3ebcec28fb71909
Dong Xia <dev12301@linx-info.com>
2023-02-06 05:09:44 +0000

    根组件文件
    Signed-off-by: Dong Xia <dev12301@linx-info.com>

---
+++ src/app.vue
@@ -0,0 +1,42 @@
+<template>
+  <a-config-provider :locale="locale">
+    <div id="app">
+      <router-view />
+      <Waiting />
+    </div>
+  </a-config-provider>
+</template>
+
+<script>
+
+import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN'
+import Waiting from '@/components/waiting'
+export default {
+  name: 'App',
+  components: {
+    Waiting
+  },
+  data() {
+    return {
+      locale: zhCN
+    }
+  },
+  created() {
+    this.$notification.config({
+      top: '100px',
+      duration: 2
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 0;
+  padding: 0;
+}
+</style>
