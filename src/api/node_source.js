601a0c215f4d4c180d43d94328b4d1d703a6e2fd
Dong Xia <dev12301@linx-info.com>
2023-02-08 09:21:43 +0000

    软件源数据接口配置文件
    Signed-off-by: Dong Xia <dev12301@linx-info.com>

---
+++ src/api/node_source.js
@@ -0,0 +1,76 @@
+import request from '../utils/request'
+
+export function getSource(data) {
+  return request({
+    url: '/node/getSourceList',
+    method: 'post',
+    data
+  })
+}
+
+export function setSource(data) {
+  return request({
+    url: '/node/setSourceList',
+    method: 'post',
+    data,
+    headers: {
+      tip: 'no'
+    }
+  })
+}
+
+export function scanSourceListOnce(data) {
+  return request({
+    url: '/node/scanSourceListOnce',
+    method: 'post',
+    data
+  })
+}
+
+export function scanSourceListCycle(data) {
+  return request({
+    url: '/node/scanSourceListCycle',
+    method: 'post',
+    data
+  })
+}
+
+export function setSourceDataBase(data) {
+  return request({
+    url: '/node/setSourceDatabase',
+    method: 'post',
+    data
+  })
+}
+
+export function getSourceDataBase(data) {
+  return request({
+    url: '/node/getSourceDatabase',
+    method: 'post',
+    data
+  })
+}
+
+export function delSourceDataBase(data) {
+  return request({
+    url: '/node/delSourceDatabaseByID',
+    method: 'post',
+    data
+  })
+}
+
+export function getScanSource(data) {
+  return request({
+    url: '/node/getScanSource',
+    method: 'post',
+    data
+  })
+}
+
+export function rollbackSourceList(data) {
+  return request({
+    url: '/node/rollbackSourceList',
+    method: 'post',
+    data
+  })
+}
