c62fc5a4bbb50b1fc81a95901cc72f6c0d7cee3a
Dong Xia <dev12301@linx-info.com>
2023-02-09 01:15:37 +0000

    节点软件包升级组件开发
    Signed-off-by: Dong Xia <dev12301@linx-info.com>

--- src/pages/node/node-layout/package/updateable_package_list.vue
+++ src/pages/node/node-layout/package/updateable_package_list.vue
@@ -13,3 +13,57 @@
     />
   </div>
 </template>
+
+<script>
+import { getUpdateablePackage } from '@/api/node_package'
+export default {
+  props: {
+    nodeId: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      list: [],
+      columns: [
+        { title: '软件包名', dataIndex: 'name', ellipsis: true },
+        { title: '当前版本', dataIndex: 'nowVersion', ellipsis: true },
+        { title: '可升级版本', dataIndex: 'newVersion', ellipsis: true }
+      ],
+      rowKeys: [],
+      selectList: [],
+      loading: true
+    }
+  },
+  created() {
+    this.getUpdateablePackage()
+  },
+  methods: {
+    getUpdateablePackage() {
+      this.loading = true
+      getUpdateablePackage(this.nodeId).then(res => {
+        this.loading = false
+        if (res.code === 200) {
+          this.list = res.data
+        }
+      }).catch(() => {
+        this.loading = false
+      })
+    },
+    onSelectChange(row, selectList) {
+      this.rowKeys = row
+      this.selectList = selectList
+    },
+    handleSubmit() {
+      return new Promise((resolve, reject) => {
+        if (this.selectList.length === 0) {
+          this.$notification.error({ message: '请选择要升级的软件包' })
+          reject()
+        }
+        resolve(this.selectList)
+      })
+    }
+  }
+}
+</script>
