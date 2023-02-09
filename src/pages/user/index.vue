aecf372827420289884c57c1f92d177d9814326f
Dong Xia <dev12301@linx-info.com>
2023-02-09 05:57:24 +0000

    获取用户列表数据接口联调以及展示
    Signed-off-by: Dong Xia <dev12301@linx-info.com>

--- src/pages/user/index.vue
+++ src/pages/user/index.vue
@@ -30,3 +30,53 @@
     </a-table>
   </div>
 </template>
+
+<script>
+import { getUserList } from '../../api/user'
+import { parseTime } from '../../utils/time'
+export default {
+  data() {
+    return {
+      loading: false,
+      tableHeight: '70vh',
+      list: [],
+      rowData: {},
+      editUserVisible: false,
+      columns: [
+        { title: '用户名称', dataIndex: 'id', ellipsis: true, scopedSlots: { customRender: 'id' }},
+        { title: '修改时间', dataIndex: 'modifyTime', ellipsis: true, customRender: (text) => { return parseTime(text) } },
+        { title: '输入密码错误次数', dataIndex: 'pwdErrorCount', ellipsis: true },
+        { title: '输入密码错误时间', dataIndex: 'lastPwdErrorTime', ellipsis: true, customRender: (text) => { if (text === '0') { return '--' } return parseTime(text) } },
+        { title: '操作', dataIndex: 'operation', scopedSlots: { customRender: 'operation' }, width: 200, align: 'center' }
+      ],
+      title: '新增用户'
+    }
+  },
+  created() {
+    this.calcTableHeight()
+    this.loadData()
+  },
+  methods: {
+    calcTableHeight() {
+      this.$nextTick(() => {
+        this.tableHeight = window.innerHeight - this.$refs['filter'].clientHeight - 135
+      })
+    },
+    loadData() {
+      this.list = []
+      this.loading = true
+      getUserList().then(res => {
+        if (res.code === 200) {
+          this.list = res.data
+        }
+      }).finally(() => {
+        this.loading = false
+      })
+    },
+    refreshTable() {
+      this.editUserVisible = false
+      this.loadData()
+    },
+  }
+}
+</script>
