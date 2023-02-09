312a54a3551fab99a4ff05f8a1218c75541cb62b
Dong Xia <dev12301@linx-info.com>
2023-02-09 01:14:35 +0000

    节点软件包升级组件
    Signed-off-by: Dong Xia <dev12301@linx-info.com>

---
+++ src/pages/node/node-layout/package/updateable_package_list.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <a-table
+      :row-selection="{ onChange: onSelectChange, selectedRowKeys: rowKeys }"
+      :data-source="list"
+      :columns="columns"
+      :row-key="record => record.name"
+      :loading="loading"
+      ok-text="升级"
+      :style="{ 'max-height': '600px', 'min-height': '250px' }"
+      :scroll="{ y: 480 }"
+      bordered
+    />
+  </div>
+</template>
