880d70bb3bf1029401dccdb5aa7dad2461c3124f
Dong Xia <dev12301@linx-info.com>
2023-02-20 09:21:18 +0000

    添加仓库管理软件包相关api配置
    Signed-off-by: Dong Xia <dev12301@linx-info.com>

--- src/api/repository.js
+++ src/api/repository.js
@@ -50,3 +50,57 @@ export function updateSourcePlan(data) {
   })
 }
 
+export function batchProcessing(data, isTip) {
+  const headers = {
+    'Content-Type': 'application/json; charset=UTF-8',
+    tip: 'no'
+  }
+  if (isTip) delete headers.tip
+  return request({
+    timeout: 0,
+    url: '/node/BatchProcessing',
+    method: 'post',
+    headers,
+    data
+  })
+}
+
+export function updateUserPkgPlan(data) {
+  return request({
+    url: '/updateUserPkgPlan',
+    method: 'post',
+    data
+  })
+}
+
+export function queryUserPkgPlan(data) {
+  return request({
+    url: '/queryUserPkgPlan',
+    method: 'post',
+    data
+  })
+}
+
+export function delUserPkgPlan(data) {
+  return request({
+    url: '/delUserPkgPlan',
+    method: 'post',
+    data
+  })
+}
+
+export function insertUserPkgPlan(data) {
+  return request({
+    url: '/updatePkgClass',
+    method: 'post',
+    data
+  })
+}
+
+export function getPkgSize(data) {
+  return request({
+    url: 'getPkgSize',
+    method: 'post',
+    data
+  })
+}
