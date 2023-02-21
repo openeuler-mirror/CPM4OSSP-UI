ac9328f6cbeb7c6016996659055ef212a3e6ffe4
Dong Xia <dev12301@linx-info.com>
2023-02-21 08:50:38 +0000

    节点选择器组件界面开发
    Signed-off-by: Dong Xia <dev12301@linx-info.com>

---
+++ src/pages/repository/source/node-select.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <a-form-model :wrapper-col="{ span: 20 }" :label-col="{ span: 4 }">
+      <a-form-model-item label="软件源模板名">
+        <a-input :value="planInfo.planName" disabled />
+      </a-form-model-item>
+      <a-form-model-item label="选择配置节点">
+        <a-select
+          v-model="selecPlanName"
+          mode="multiple"
+        >
+          <a-select-option v-for="item in nodeList" :key="item.id">
+            {{ item.name }}
+          </a-select-option>
+        </a-select>
+      </a-form-model-item>
+    </a-form-model>
+  </div>
+</template>
+
+<script>
+import { getNodeList, getNodeStatus } from '@/api/node'
+import { batchProcessing } from '@/api/repository'
+export default {
+  props: {
+    planInfo: {
+      type: Object,
+      default: function() {
+        return {}
+      }
+    }
+  },
+  data() {
+    return {
+      selecPlanName: [],
+      nodeList: []
+    }
+  },
+  created() {
+    this.getNodeList()
+  },
+}
+</script>
