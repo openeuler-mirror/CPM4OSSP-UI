802cb660cf73c78e877d6d5d58a80b4b9ae7dff8
Dong Xia <dev12301@linx-info.com>
2023-03-02 01:47:22 +0000

    节点软件源模块mutations的定义
    Signed-off-by: Dong Xia <dev12301@linx-info.com>

--- src/store/modules/node-source.js
+++ src/store/modules/node-source.js
@@ -2,6 +2,23 @@ const nodeSource = {
   state: {
     nodeCheckSetting: []
   },
+  mutations: {
+    SET_CHECK_SOURCE_INTERVAL(state, nodeId) {
+      const obj = {
+        nodeId,
+        time: 30
+      }
+      obj.interval = setInterval(() => {
+        obj.time--
+        if (obj.time === 0 && obj.interval !== undefined) {
+          clearInterval(obj.interval)
+          const nodeIndex = state.nodeCheckSetting.findIndex(item => item.nodeId === nodeId)
+          state.nodeCheckSetting.splice(nodeIndex, 1)
+        }
+      }, 1000)
+      state.nodeCheckSetting.push(obj)
+    }
+  },
 }
 
 export default nodeSource
