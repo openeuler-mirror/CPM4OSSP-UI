1e0699692d6de05d20aff36a2ffd38ea3c9ec98d
Dong Xia <dev12301@linx-info.com>
2023-03-02 09:28:44 +0000

    自定义可拖拽表格组件界面
    Signed-off-by: Dong Xia <dev12301@linx-info.com>

---
+++ src/components/draggable-table/index.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+  name: 'DraggabledTable',
+  render() {
+    const { $slots, $scopedSlots, $attrs, $props, $listeners } = this
+    const props = { ...$attrs, ...$props }
+    const resizeableTitle = {
+      header: {
+        cell(h, props, children) {
+          const { key, ...restProps } = props
+          const col = $attrs.columns.find((col) => {
+            const k = col.dataIndex || col.key
+            return k === key
+          })
+          if (!col?.width) {
+            return <th {...restProps}>{children}</th>
+          }
+          const onDrag = (x) => {
+            col.width = Math.max(x, 50)
+          }
+          return (
+            <th {...restProps} width={col.width} class='resize-table-th'>
+              {children}
+              <vue-draggable-resizable
+                style={{ transform: `translate(${col.width}px, 0px)` }}
+                key={col.key}
+                class='table-draggable-handle'
+                w={10}
+                x={col.width}
+                z={1}
+                axis='x'
+                draggable={true}
+                resizable={false}
+                onDragging={onDrag}
+              />
+            </th>
+          )
+        }
+      }
+    }
+
+  }
+}
+</script>
