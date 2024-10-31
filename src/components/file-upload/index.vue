f56e9b1ccd40ce1a1f1b0868d8f8604cfca897e6
Dong Xia <dev12301@linx-info.com>
2024-10-31 11:02:36 +0800

    文件上传组件界面开发

---
+++ src/components/file-upload/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="">
+    <a-input-group compact>
+      <a-input style="width: 85%" class="upload-input" placeholder="请选择文件" disabled />
+      <a-upload
+        style="width: 15%"
+        :custom-request="customRequest"
+      >
+        <a-button><a-icon type="upload" />选择文件</a-button>
+      </a-upload>
+    </a-input-group>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    url: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      upLoadUrl: '',
+      headers: {
+        authorization: 'authorization-text'
+      }
+    }
+  },
+  methods: {
+    customRequest(option) {
+      const { onError, onSuccess, file } = option
+      const formData = new FormData()
+      formData.append('file', file)
+    }
+  }
+}
+</script>
+
