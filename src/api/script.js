d3641afdf25853f4e84e7d94772405746d17bc3a
Dong Xia <dev12301@linx-info.com>
2024-10-31 13:14:45 +0800

    完成脚本下发功能开发

--- src/api/script.js
+++ src/api/script.js
@@ -1,11 +1,31 @@
 import request from './config'
 
+// 获取脚本列表
+export function getScriptListApi(data) {
+  return request({
+    url: '/script/list',
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    data
+  })
+}
+
 // 上传脚本
 export function uploadScriptApi(data) {
   return request({
     url: '/script/upload',
     method: 'post',
     headers: { 'Content-Type': 'multipart/form-data;charset=UTF-8' },
+    data
+  })
+}
+
+// 下发脚本
+export function executeScriptApi(data) {
+  return request({
+    url: '/script/execute',
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
     data
   })
 }
