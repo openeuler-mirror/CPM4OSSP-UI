54e55eb8997da6ae094305801a3d59cc8d2222fb
Dong Xia <dev12301@linx-info.com>
2024-11-29 13:06:01 +0800

    自定义loading指令优化

--- src/background.js
+++ src/background.js
@@ -11,51 +11,55 @@ protocol.registerSchemesAsPrivileged([
 ])
 let mainWin = null
 async function createWindow() {
-  mainWin = new BrowserWindow({
-    width: 1400,
-    height: 1000,
-    show: false,
-    backgroundColor: '#2e2c29',
-    title: '系统软件安装管理平台',
-    webPreferences: {
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
-      contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION,
-      webSecurity: false 
-    }
-  })
-  const menuTemplate = Menu.buildFromTemplate([
-    {
-      label: '返回&(Ctrl+B)',
-      accelerator: 'Ctrl+B',
-      click: () => {
-        mainWin.webContents.goBack()
+  try{
+    mainWin = new BrowserWindow({
+      width: 1400,
+      height: 1000,
+      show: false,
+      backgroundColor: '#2e2c29',
+      title: '系统软件安装管理平台',
+      webPreferences: {
+        nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
+        contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION,
+        webSecurity: false 
       }
-    },
-    {
-      label: '全屏&(Ctrl+F)',
-      accelerator: 'Ctrl+F',
-      role: 'togglefullscreen'
-    },
-    {
-      label: '退出&(Ctrl+Q)',
-      accelerator: 'Ctrl+Q',
-      role: 'close'
-    },
-    {
-      label: '强制刷新&(Ctrl+S)',
-      accelerator: 'Ctrl+S',
-      role: 'reload'
+    })
+    const menuTemplate = Menu.buildFromTemplate([
+      {
+        label: '返回&(Ctrl+B)',
+        accelerator: 'Ctrl+B',
+        click: () => {
+          mainWin.webContents.goBack()
+        }
+      },
+      {
+        label: '全屏&(Ctrl+F)',
+        accelerator: 'Ctrl+F',
+        role: 'togglefullscreen'
+      },
+      {
+        label: '退出&(Ctrl+Q)',
+        accelerator: 'Ctrl+Q',
+        role: 'close'
+      },
+      {
+        label: '强制刷新&(Ctrl+S)',
+        accelerator: 'Ctrl+S',
+        role: 'reload'
+      }
+    ])
+    Menu.setApplicationMenu(menuTemplate)
+    mainWin.once('ready-to-show', () => {
+      mainWin.maximize()
+      mainWin.show()
+    })
+    createProtocol('app')
+    mainWin.loadURL('app://./index.html')
+    toOpenDevTools = function() {
+      mainWin.webContents.openDevTools()
     }
-  ])
-  Menu.setApplicationMenu(menuTemplate)
-  mainWin.once('ready-to-show', () => {
-    mainWin.maximize()
-    mainWin.show()
-  })
-  createProtocol('app')
-  mainWin.loadURL('app://./index.html')
-  toOpenDevTools = function() {
-    mainWin.webContents.openDevTools()
+  } catch(error) {
+    console.log(error)
   }
 }
 
