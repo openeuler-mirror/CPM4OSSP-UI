2fb30eaad96908315599aa6f351cc151818f089d
Dong Xia <dev12301@linx-info.com>
2024-12-30 15:25:38 +0800

    修复节点软件源配置检测异常问题

--- src/store/modules/node-source.js
+++ src/store/modules/node-source.js
@@ -2,27 +2,33 @@
 const nodeSource = {
   state: {
     // 存放节点，检测源配置30秒后才能再次操作
-    nodeCheckSetting: []
+    nodeCheckSetting: new Map()
   },
   getters: {
 
   },
   mutations: {
-    // 倒计时30秒
     SET_CHECK_SOURCE_INTERVAL(state, nodeId) {
+      console.log(`Setting check source interval for node ${nodeId}`)
+  
+      if (state.nodeCheckSetting.has(nodeId)) {
+        console.log(`Clearing existing interval for node ${nodeId}`)
+        clearTimeout(state.nodeCheckSetting.get(nodeId).timeout)
+        state.nodeCheckSetting.delete(nodeId)
+      }
+  
       const obj = {
         nodeId,
         time: 30
       }
-      obj.interval = setInterval(() => {
-        obj.time--
-        if (obj.time === 0 && obj.interval !== undefined) {
-          clearInterval(obj.interval)
-          const nodeIndex = state.nodeCheckSetting.findIndex(item => item.nodeId === nodeId)
-          state.nodeCheckSetting.splice(nodeIndex, 1)
-        }
-      }, 1000)
-      state.nodeCheckSetting.push(obj)
+  
+      obj.timeout = setTimeout(() => {
+        console.log(`Interval finished for node ${nodeId}`)
+        state.nodeCheckSetting.delete(nodeId)
+      }, 30000)
+  
+      state.nodeCheckSetting.set(nodeId, obj)
+      console.log(`Interval set for node ${nodeId}`)
     }
   },
   actions: {
