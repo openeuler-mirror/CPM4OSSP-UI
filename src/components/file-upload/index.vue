74f843938c6dff35419de3be55515ea88ec2be85
Dong Xia <dev12301@linx-info.com>
2024-12-30 15:46:01 +0800

    修复文件上传组件异常问题

--- src/components/file-upload/index.vue
+++ src/components/file-upload/index.vue
@@ -1,12 +1,22 @@
 <template>
-  <div class="">
+  <div class="file-upload-container">
     <a-input-group compact>
-      <a-input style="width: 85%" class="upload-input" placeholder="请选择文件" disabled />
+      <a-input
+        style="width: 85%"
+        class="upload-input"
+        :placeholder="placeholderText"
+        v-model="selectedFileName"
+        disabled
+      />
       <a-upload
         style="width: 15%"
-        :custom-request="customRequest"
+        :custom-request="handleUpload"
+        :action="url"
+        :headers="headers"
       >
-        <a-button><a-icon type="upload" />选择文件</a-button>
+        <a-button type="primary">
+          <a-icon type="upload" />选择文件
+        </a-button>
       </a-upload>
     </a-input-group>
   </div>
@@ -19,39 +29,61 @@ export default {
   props: {
     url: {
       type: String,
-      default: ''
+      required: true
+    },
+    placeholderText: {
+      type: String,
+      default: '请选择文件'
     }
   },
   data() {
     return {
-      upLoadUrl: '',
+      selectedFileName: '',
       headers: {
         authorization: 'authorization-text'
       }
     }
   },
   methods: {
-    customRequest(option) {
+    async handleUpload(option) {
       const { onError, onSuccess, file } = option
-      const formData = new FormData()
-      formData.append('file', file)
-      uploadScriptApi(formData)
-        .then((res) => {
-          if (res.code === 200) {
-            onSuccess(res)
-            this.$emit('on-success', res)
-          } else {
-            onError(res.data.msg)
-          }
-        })
-        .catch(err => {
-          console.error(err)
-        })
+      this.selectedFileName = file.name
+
+      try {
+        const formData = new FormData()
+        formData.append('file', file)
+
+        const response = await uploadScriptApi(formData)
+        if (response.code === 200) {
+          onSuccess(response)
+          this.$emit('on-success', response)
+        } else {
+          this.handleError(response.data.msg)
+        }
+      } catch (error) {
+        console.error(error)
+        this.handleError('上传文件时发生错误')
+      }
+    },
+    handleError(message) {
+      // 使用全局错误处理或通知组件展示错误信息
+      this.$message.error(message)
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.file-upload-container {
+  display: flex;
+  align-items: center;
 
+  .upload-input {
+    margin-right: 8px;
+  }
+
+  .ant-btn {
+    width: 100%;
+  }
+}
 </style>
